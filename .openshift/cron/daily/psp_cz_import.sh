44c7cc093a0492681d723eb819068c97c301df52
Libor Nenadál <libor.nenadal@gmail.com>
2012-10-21 22:55:11 +0200

    Fixed importer - moved to proper place.

--- .openshift/cron/daily/psp_cz_import.sh
+++ .openshift/cron/daily/psp_cz_import.sh
@@ -4,4 +4,4 @@
 source $OPENSHIFT_GEAR_DIR/virtenv/bin/activate
 
 echo "Executing import script."
-python "$OPENSHIFT_REPO_DIR"psp_cz_import.py -d $OPENSHIFT_TMP_DIR
+python "$OPENSHIFT_REPO_DIR"wsgi/psp_cz_import.py -d $OPENSHIFT_TMP_DIR
