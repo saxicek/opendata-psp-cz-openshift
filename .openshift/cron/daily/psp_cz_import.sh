c39b4f9e239c9f3329510d1d434471f22262a0fc
Libor Nenadál <libor.nenadal@gmail.com>
2012-10-22 12:11:38 +0200

    Daily import script logs everything into the temp dir.

--- .openshift/cron/daily/psp_cz_import.sh
+++ .openshift/cron/daily/psp_cz_import.sh
@@ -5,4 +5,4 @@ source $OPENSHIFT_GEAR_DIR/virtenv/bin/activate
 export DJANGO_SETTINGS_MODULE=openshift.settings
 
 echo "Executing import script."
-python "$OPENSHIFT_REPO_DIR"wsgi/psp_cz_import.py -d $OPENSHIFT_TMP_DIR
+python "$OPENSHIFT_REPO_DIR"wsgi/psp_cz_import.py -d $OPENSHIFT_TMP_DIR >> "$OPENSHIFT_TMP_DIR"psp_cz_import_`date +%Y-%m-%d`.log 2>&1
